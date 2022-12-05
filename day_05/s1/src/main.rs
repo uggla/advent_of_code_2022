6e72653074b51259afb10ef4a11894a698e9fafc
Uggla <uggla@free.fr>
2022-12-06 00:40:01 +0100

    feat: Solutions for day 05

--- day_05/s1/src/main.rs
+++ day_05/s1/src/main.rs
@@ -1,5 +1,3 @@
-use std::fmt::Write as _;
-
 fn parse_input(input: Option<&str>) -> Vec<String> {
     let input = match input {
         None => include_str!("../../input.txt"),
@@ -40,15 +38,16 @@ fn parse(input: Vec<String>) -> (Map, Actions) {
 fn parse_map(input: &[String]) -> Vec<Vec<String>> {
     // Get number of element
     let mut input = input.to_vec();
-    let nb_elem = input
+    let nb_elem = dbg!(input
         .pop()
         .unwrap()
         .split("   ")
         .collect::<Vec<&str>>()
         .last()
         .unwrap()
+        .trim()
         .parse::<usize>()
-        .unwrap();
+        .unwrap());
 
     let mut map: Vec<Vec<String>> = Vec::new();
     for _ in 0..nb_elem {
@@ -67,25 +66,26 @@ fn parse_map(input: &[String]) -> Vec<Vec<String>> {
             }
             // dbg!(&case);
             // dbg!(&remaining);
-            item.push(case.replace(['[', ']'], "").replace("   ", " ").to_string());
+            if case != "   " {
+                item.push(case.replace(['[', ']'], "").to_string());
+            }
         }
+    }
+    for item in map.iter_mut() {
+        item.reverse();
     }
     map
 }
 
-fn get_map(map: Vec<Vec<String>>) -> String {
+fn get_map(map: &Vec<Vec<String>>) -> String {
     let mut line = Vec::new();
     let height = map.iter().map(|x| x.len()).max().unwrap();
     for case in 0..height {
         let mut column = Vec::new();
         for stack in 0..map.len() {
             column.push(match map.get(stack).unwrap().get(case) {
-                Some(x) => match x.as_str() {
-                    " " => "   ".to_string(),
-                    _ => format!("[{}]", x),
-                },
-
-                None => unreachable!(),
+                Some(x) => format!("[{}]", x),
+                None => "   ".to_string(),
             });
         }
         line.push(column)
@@ -93,6 +93,7 @@ fn get_map(map: Vec<Vec<String>>) -> String {
 
     let out = line
         .iter()
+        .rev()
         .map(|x| x.join(" ").trim_end().to_string())
         .collect::<Vec<String>>()
         .join("\n");
@@ -100,11 +101,37 @@ fn get_map(map: Vec<Vec<String>>) -> String {
     out
 }
 
+fn top_case(map: Map) -> String {
+    let top_case = map
+        .iter()
+        .map(|x| {
+            x.iter()
+                .filter(|x| !x.contains(' '))
+                .last()
+                .unwrap()
+                .to_string()
+        })
+        .collect::<Vec<String>>()
+        .join("");
+
+    top_case
+}
+
+fn move_case(map: &mut Map, nb: usize, src: usize, dst: usize) {
+    let src_stack = map.get(src - 1).unwrap();
+    let (lhs, rhs) = src_stack.split_at(src_stack.len() - nb);
+    let lhs = lhs.to_vec();
+    let mut rhs = rhs.to_vec();
+    rhs.reverse();
+    map[src - 1] = lhs;
+    map[dst - 1].append(&mut rhs);
+}
+
 #[derive(Debug, PartialEq)]
 struct Action {
-    nb: u32,
-    src: u32,
-    dst: u32,
+    nb: usize,
+    src: usize,
+    dst: usize,
 }
 
 fn parse_actions(input: &[String]) -> Vec<Action> {
@@ -134,7 +161,15 @@ fn parse_actions(input: &[String]) -> Vec<Action> {
 }
 
 fn run(input: Vec<String>) -> String {
-    todo!();
+    let (mut map, actions) = parse(input);
+    println!("{}", get_map(&map));
+    println!("---------------------------------------------------");
+    for action in actions {
+        move_case(&mut map, action.nb, action.src, action.dst);
+        println!("{}", get_map(&map));
+        println!("---------------------------------------------------");
+    }
+    top_case(map)
 }
 
 fn main() {
@@ -171,10 +206,9 @@ mod tests {
 
         let mut input_lines = parse_input(Some(input));
         let map = parse_map(&input_lines);
-        let map = get_map(map);
-        println!("{}", map);
+        println!("{}", get_map(&map));
         input_lines.pop();
-        assert_eq!(input_lines.join("\n"), map);
+        assert_eq!(input_lines.join("\n"), get_map(&map));
     }
 
     #[test]
@@ -190,10 +224,9 @@ mod tests {
 
         let mut input_lines = parse_input(Some(input));
         let map = parse_map(&input_lines);
-        let map = get_map(map);
-        println!("{}", map);
+        println!("{}", get_map(&map));
         input_lines.pop();
-        assert_eq!(input_lines.join("\n"), map);
+        assert_eq!(input_lines.join("\n"), get_map(&map));
     }
 
     #[test]
@@ -210,10 +243,84 @@ mod tests {
 
         let mut input_lines = parse_input(Some(input));
         let map = parse_map(&input_lines);
-        let map = get_map(map);
-        println!("{}", map);
+        println!("{}", get_map(&map));
         input_lines.pop();
-        assert_eq!(input_lines.join("\n"), map);
+        assert_eq!(input_lines.join("\n"), get_map(&map));
+    }
+
+    #[test]
+    fn test_top_case() {
+        let input = indoc!(
+            "
+                        [D]
+                [D]     [C]
+            [N] [C]     [B]
+            [Z] [M] [P] [A]
+             1   2   3   4
+            "
+        );
+
+        let input_lines = parse_input(Some(input));
+        let map = parse_map(&input_lines);
+        assert_eq!(top_case(map), "NDPD".to_string());
+    }
+
+    #[test]
+    fn test_move_case_01() {
+        let src = indoc!(
+            "
+                        [H]
+                [D]     [G]
+            [N] [C]     [B]
+            [Z] [M] [P] [A]
+             1   2   3   4
+            "
+        );
+        let dst = indoc!(
+            "
+                        [C]
+                        [D]
+                        [H]
+                        [G]
+            [N]         [B]
+            [Z] [M] [P] [A]"
+        );
+
+        let input_lines = parse_input(Some(src));
+        let mut map = parse_map(&input_lines);
+        println!("{}", get_map(&map));
+        move_case(&mut map, 2, 2, 4);
+        let dst_map = get_map(&map);
+        println!("{}", get_map(&map));
+        assert_eq!(dst, dst_map)
+    }
+
+    #[test]
+    fn test_move_case_02() {
+        let src = indoc!(
+            "
+                        [H]
+                [D]     [G]
+            [N] [C]     [B]
+            [Z] [M] [P] [A]
+             1   2   3   4
+            "
+        );
+        let dst = indoc!(
+            "
+                        [H]
+                [D]     [G]
+                [C] [N] [B]
+            [Z] [M] [P] [A]"
+        );
+
+        let input_lines = parse_input(Some(src));
+        let mut map = parse_map(&input_lines);
+        println!("{}", get_map(&map));
+        move_case(&mut map, 1, 1, 3);
+        let dst_map = get_map(&map);
+        println!("{}", get_map(&map));
+        assert_eq!(dst, dst_map)
     }
 
     #[test]
@@ -258,26 +365,6 @@ mod tests {
     }
 
     #[test]
-    fn test_parse() {
-        let input = parse_input(Some(indoc!(
-            "
-                [D]
-            [N] [C]
-            [Z] [M] [P]
-             1   2   3
-
-            move 1 from 2 to 1
-            move 3 from 1 to 3
-            move 2 from 2 to 1
-            move 1 from 1 to 2
-            "
-        )));
-        let (map, actions) = parse(input);
-
-        assert_eq!(1, 0);
-    }
-
-    #[test]
     fn test_run() {
         let input = parse_input(Some(indoc!(
             "
@@ -295,5 +382,27 @@ mod tests {
         dbg!(&input);
         let answer = run(input);
         assert_eq!(answer, "CMZ".to_string());
+    }
+
+    #[test]
+    fn test_parse_big_map() {
+        let input = parse_input(Some(indoc!(
+            "
+                    [H]         [S]         [D]
+                [S] [C]         [C]     [Q] [L]
+                [C] [R] [Z]     [R]     [H] [Z]
+                [G] [N] [H] [S] [B]     [R] [F]
+            [D] [T] [Q] [F] [Q] [Z]     [Z] [N]
+            [Z] [W] [F] [N] [F] [W] [J] [V] [G]
+            [T] [R] [B] [C] [L] [P] [F] [L] [H]
+            [H] [Q] [P] [L] [G] [V] [Z] [D] [B]
+             1   2   3   4   5   6   7   8   9 
+
+            move 2 from 7 to 2
+            "
+        )));
+        dbg!(&input);
+        let answer = run(input);
+        assert_eq!(answer, "DFHZSSZQD".to_string());
     }
 }
--- day_05/s2/src/main.rs
+++ day_05/s2/src/main.rs
@@ -13,74 +13,164 @@ fn parse_input(input: Option<&str>) -> Vec<String> {
     output
 }
 
-#[derive(PartialEq, Debug)]
-struct Elf {
-    clean_section_start: u32,
-    clean_section_end: u32,
-}
+type Map = Vec<Vec<String>>;
+type Actions = Vec<Action>;
 
-impl Elf {
-    fn new(clean_section_start: u32, clean_section_end: u32) -> Self {
-        Self {
-            clean_section_start,
-            clean_section_end,
+fn parse(input: Vec<String>) -> (Map, Actions) {
+    // Split vec in 2 parts (map, actions)
+    let mut delim_line = 0;
+    for (index, line) in input.iter().enumerate() {
+        if line.is_empty() {
+            delim_line = index;
         }
     }
+
+    assert!(delim_line != 0);
+
+    let (map, actions) = input.split_at(delim_line);
+    dbg!(&map);
+    dbg!(&actions);
+    let map = parse_map(map);
+    let actions = parse_actions(actions);
+    (map, actions)
 }
 
-#[derive(PartialEq, Debug)]
-struct Elfpair {
-    clean_section_start: u32,
-    clean_section_end: u32,
-}
+fn parse_map(input: &[String]) -> Vec<Vec<String>> {
+    // Get number of element
+    let mut input = input.to_vec();
+    let nb_elem = dbg!(input
+        .pop()
+        .unwrap()
+        .split("   ")
+        .collect::<Vec<&str>>()
+        .last()
+        .unwrap()
+        .trim()
+        .parse::<usize>()
+        .unwrap());
 
-impl Elfpair {
-    fn new(clean_section_start: u32, clean_section_end: u32) -> Self {
-        Self {
-            clean_section_start,
-            clean_section_end,
-        }
+    let mut map: Vec<Vec<String>> = Vec::new();
+    for _ in 0..nb_elem {
+        map.push(Vec::new());
     }
-}
 
-fn is_section_contained(elf: Elf, elfpair: Elfpair) -> bool {
-    if elf.clean_section_end < elfpair.clean_section_start
-        || elfpair.clean_section_end < elf.clean_section_start
-    {
-        return false;
-    }
-
-    true
-}
-
-fn parse_line(line: &str) -> (Elf, Elfpair) {
-    let elves: Vec<&str> = line.split(',').collect();
-    let clean_sections: Vec<&str> = elves[0].split('-').collect();
-    let elf = Elf::new(
-        clean_sections[0].parse().unwrap(),
-        clean_sections[1].parse().unwrap(),
-    );
-
-    let clean_sections: Vec<&str> = elves[1].split('-').collect();
-    let elfpair = Elfpair::new(
-        clean_sections[0].parse().unwrap(),
-        clean_sections[1].parse().unwrap(),
-    );
-
-    (elf, elfpair)
-}
-
-fn run(input: Vec<String>) -> u32 {
-    let mut output: u32 = 0;
     for line in input {
-        dbg!(&line);
-        let (elf, elfpair) = parse_line(&line);
-        if is_section_contained(elf, elfpair) {
-            dbg!("contained");
-            output += 1;
+        #[allow(unused_assignments)]
+        let (mut case, mut remaining) = line.split_at(0);
+        for item in map.iter_mut().take(nb_elem) {
+            (case, remaining) = remaining.split_at(3);
+            if !remaining.is_empty() {
+                remaining = remaining.strip_prefix(' ').unwrap();
+            } else {
+                remaining = "   ";
+            }
+            // dbg!(&case);
+            // dbg!(&remaining);
+            if case != "   " {
+                item.push(case.replace(['[', ']'], "").to_string());
+            }
         }
     }
-    output
+
+    for item in map.iter_mut() {
+        item.reverse();
+    }
+    map
+}
+
+fn get_map(map: &Vec<Vec<String>>) -> String {
+    let mut line = Vec::new();
+    let height = map.iter().map(|x| x.len()).max().unwrap();
+    for case in 0..height {
+        let mut column = Vec::new();
+        for stack in 0..map.len() {
+            column.push(match map.get(stack).unwrap().get(case) {
+                Some(x) => format!("[{}]", x),
+                None => "   ".to_string(),
+            });
+        }
+        line.push(column)
+    }
+
+    let out = line
+        .iter()
+        .rev()
+        .map(|x| x.join(" ").trim_end().to_string())
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    out
+}
+
+fn top_case(map: Map) -> String {
+    let top_case = map
+        .iter()
+        .map(|x| {
+            x.iter()
+                .filter(|x| !x.contains(' '))
+                .last()
+                .unwrap()
+                .to_string()
+        })
+        .collect::<Vec<String>>()
+        .join("");
+
+    top_case
+}
+
+fn move_case(map: &mut Map, nb: usize, src: usize, dst: usize) {
+    let src_stack = map.get(src - 1).unwrap();
+    let (lhs, rhs) = src_stack.split_at(src_stack.len() - nb);
+    let lhs = lhs.to_vec();
+    let mut rhs = rhs.to_vec();
+    // rhs.reverse();
+    map[src - 1] = lhs;
+    map[dst - 1].append(&mut rhs);
+}
+
+#[derive(Debug, PartialEq)]
+struct Action {
+    nb: usize,
+    src: usize,
+    dst: usize,
+}
+
+fn parse_actions(input: &[String]) -> Vec<Action> {
+    let mut input = input.to_vec();
+    input.remove(0);
+
+    let actions = input
+        .iter_mut()
+        .map(|x| {
+            x.replace("move", "")
+                .replace("from", ",")
+                .replace("to", ",")
+        })
+        .collect::<Vec<String>>()
+        .iter()
+        .map(|x| {
+            let v = x.split(',').collect::<Vec<&str>>();
+            Action {
+                nb: v[0].trim().parse().unwrap(),
+                src: v[1].trim().parse().unwrap(),
+                dst: v[2].trim().parse().unwrap(),
+            }
+        })
+        .collect::<Vec<Action>>();
+
+    actions
+}
+
+fn run(input: Vec<String>) -> String {
+    let (mut map, actions) = parse(input);
+    println!("{}", get_map(&map));
+    println!("---------------------------------------------------");
+    for action in actions {
+        move_case(&mut map, action.nb, action.src, action.dst);
+        println!("{}", get_map(&map));
+        println!("---------------------------------------------------");
+    }
+    top_case(map)
 }
 
 fn main() {
@@ -105,68 +195,215 @@ mod tests {
     }
 
     #[test]
-    fn test_is_section_contained_01() {
-        let elf = Elf::new(2, 4);
-        let elfpair = Elfpair::new(6, 8);
-        assert_eq!(is_section_contained(elf, elfpair), false);
+    fn test_parse_map_01() {
+        let input = indoc!(
+            "
+                [D]
+            [N] [C]
+            [Z] [M] [P]
+             1   2   3
+            "
+        );
+
+        let mut input_lines = parse_input(Some(input));
+        let map = parse_map(&input_lines);
+        println!("{}", get_map(&map));
+        input_lines.pop();
+        assert_eq!(input_lines.join("\n"), get_map(&map));
     }
 
     #[test]
-    fn test_is_section_contained_02() {
-        let elf = Elf::new(2, 3);
-        let elfpair = Elfpair::new(4, 5);
-        assert_eq!(is_section_contained(elf, elfpair), false);
+    fn test_parse_map_02() {
+        let input = indoc!(
+            "
+                [D]
+            [N] [C]
+            [Z] [M] [P]
+             1   2   3   4
+            "
+        );
+
+        let mut input_lines = parse_input(Some(input));
+        let map = parse_map(&input_lines);
+        println!("{}", get_map(&map));
+        input_lines.pop();
+        assert_eq!(input_lines.join("\n"), get_map(&map));
     }
 
     #[test]
-    fn test_is_section_contained_03() {
-        let elf = Elf::new(5, 7);
-        let elfpair = Elfpair::new(7, 9);
-        assert_eq!(is_section_contained(elf, elfpair), true);
+    fn test_parse_map_03() {
+        let input = indoc!(
+            "
+                        [D]
+                [D]     [C]
+            [N] [C]     [B]
+            [Z] [M] [P] [A]
+             1   2   3   4
+            "
+        );
+
+        let mut input_lines = parse_input(Some(input));
+        let map = parse_map(&input_lines);
+        println!("{}", get_map(&map));
+        input_lines.pop();
+        assert_eq!(input_lines.join("\n"), get_map(&map));
     }
 
     #[test]
-    fn test_is_section_contained_04() {
-        let elf = Elf::new(2, 8);
-        let elfpair = Elfpair::new(3, 7);
-        assert_eq!(is_section_contained(elf, elfpair), true);
+    fn test_top_case() {
+        let input = indoc!(
+            "
+                        [D]
+                [D]     [C]
+            [N] [C]     [B]
+            [Z] [M] [P] [A]
+             1   2   3   4
+            "
+        );
+
+        let input_lines = parse_input(Some(input));
+        let map = parse_map(&input_lines);
+        assert_eq!(top_case(map), "NDPD".to_string());
     }
 
     #[test]
-    fn test_is_section_contained_05() {
-        let elf = Elf::new(6, 6);
-        let elfpair = Elfpair::new(4, 6);
-        assert_eq!(is_section_contained(elf, elfpair), true);
+    fn test_move_case_01() {
+        let src = indoc!(
+            "
+                        [H]
+                [D]     [G]
+            [N] [C]     [B]
+            [Z] [M] [P] [A]
+             1   2   3   4
+            "
+        );
+        let dst = indoc!(
+            "
+                        [D]
+                        [C]
+                        [H]
+                        [G]
+            [N]         [B]
+            [Z] [M] [P] [A]"
+        );
+
+        let input_lines = parse_input(Some(src));
+        let mut map = parse_map(&input_lines);
+        println!("{}", get_map(&map));
+        move_case(&mut map, 2, 2, 4);
+        let dst_map = get_map(&map);
+        println!("{}", get_map(&map));
+        assert_eq!(dst, dst_map)
     }
 
     #[test]
-    fn test_is_section_contained_06() {
-        let elf = Elf::new(2, 6);
-        let elfpair = Elfpair::new(4, 8);
-        assert_eq!(is_section_contained(elf, elfpair), true);
+    fn test_move_case_02() {
+        let src = indoc!(
+            "
+                        [H]
+                [D]     [G]
+            [N] [C]     [B]
+            [Z] [M] [P] [A]
+             1   2   3   4
+            "
+        );
+        let dst = indoc!(
+            "
+                        [H]
+                [D]     [G]
+                [C] [N] [B]
+            [Z] [M] [P] [A]"
+        );
+
+        let input_lines = parse_input(Some(src));
+        let mut map = parse_map(&input_lines);
+        println!("{}", get_map(&map));
+        move_case(&mut map, 1, 1, 3);
+        let dst_map = get_map(&map);
+        println!("{}", get_map(&map));
+        assert_eq!(dst, dst_map)
     }
 
     #[test]
-    fn test_parse_line() {
-        let elf = Elf::new(1, 2);
-        let elfpair = Elfpair::new(2, 3);
-        assert_eq!(parse_line("1-2,2-3"), (elf, elfpair));
+    fn test_parse_actions_01() {
+        let input = indoc!(
+            "
+
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2
+            "
+        );
+
+        let input_lines = parse_input(Some(input));
+        let actions = parse_actions(&input_lines);
+        assert_eq!(
+            actions,
+            vec![
+                Action {
+                    nb: 1,
+                    src: 2,
+                    dst: 1
+                },
+                Action {
+                    nb: 3,
+                    src: 1,
+                    dst: 3
+                },
+                Action {
+                    nb: 2,
+                    src: 2,
+                    dst: 1
+                },
+                Action {
+                    nb: 1,
+                    src: 1,
+                    dst: 2
+                }
+            ]
+        );
     }
 
     #[test]
     fn test_run() {
         let input = parse_input(Some(indoc!(
             "
-            2-4,6-8
-            2-3,4-5
-            5-7,7-9
-            2-8,3-7
-            6-6,4-6
-            2-6,4-8
+                [D]
+            [N] [C]
+            [Z] [M] [P]
+             1   2   3
+
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2
             "
         )));
         dbg!(&input);
         let answer = run(input);
-        assert_eq!(answer, 4);
+        assert_eq!(answer, "MCD".to_string());
     }
+
+    // #[test]
+    // fn test_parse_big_map() {
+    //     let input = parse_input(Some(indoc!(
+    //         "
+    //                 [H]         [S]         [D]
+    //             [S] [C]         [C]     [Q] [L]
+    //             [C] [R] [Z]     [R]     [H] [Z]
+    //             [G] [N] [H] [S] [B]     [R] [F]
+    //         [D] [T] [Q] [F] [Q] [Z]     [Z] [N]
+    //         [Z] [W] [F] [N] [F] [W] [J] [V] [G]
+    //         [T] [R] [B] [C] [L] [P] [F] [L] [H]
+    //         [H] [Q] [P] [L] [G] [V] [Z] [D] [B]
+    //          1   2   3   4   5   6   7   8   9
+    //
+    //         move 2 from 7 to 2
+    //         "
+    //     )));
+    //     dbg!(&input);
+    //     let answer = run(input);
+    //     assert_eq!(answer, "DFHZSSZQD".to_string());
+    // }
 }
