2c3e6b539993f94156834d7360e4a9bea2bf402e
René Ribaud <rribaud@redhat.com>
2022-12-14 22:16:34 +0100

    feat: Solutions for day 10
    - I have cheated on part 2 by looking at a solution.
      Because I did not read correctly, the divided by three was removed.
      So I did not find the solution, and I had to look at someone
      else code.

---
+++ day_11/s2/src/main.rs
@@ -0,0 +1,274 @@
+use std::collections::VecDeque;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::multispace1,
+    multi::separated_list1,
+    sequence::{delimited, pair},
+    *,
+};
+
+fn read_input(input: Option<&str>) -> String {
+    let input = match input {
+        None => include_str!("../../input.txt"),
+        Some(x) => x,
+    };
+
+    input.to_string()
+}
+
+#[derive(Debug, PartialEq)]
+enum Operation {
+    Add((Value, Value)),
+    Mul((Value, Value)),
+}
+
+#[derive(Debug, PartialEq)]
+enum Throw {
+    True(u64),
+    False(u64),
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Value {
+    Old,
+    N(u64),
+}
+
+#[derive(Debug, PartialEq)]
+struct Monkey {
+    id: u64,
+    items: VecDeque<u64>,
+    operation: Operation,
+    divisible: u64,
+    throws: (Throw, Throw),
+    thrown_count: usize,
+}
+
+fn parse_operation(input: &str) -> IResult<&str, Operation> {
+    let (input, op) = alt((tag("*"), tag("+")))(input)?;
+    let (input, _) = multispace1(input)?;
+    let (input, val) = alt((
+        tag("old").map(|_| Value::Old),
+        nom::character::complete::u64.map(Value::N),
+    ))(input)?;
+
+    let op = match op {
+        "*" => Operation::Mul((Value::Old, val)),
+        "+" => Operation::Add((Value::Old, val)),
+        _ => unreachable!(),
+    };
+
+    Ok((input, op))
+}
+
+fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
+    // Ouch nom is really cool !
+    let (input, (_, id)) = delimited(
+        tag("Monkey"),
+        pair(multispace1, nom::character::complete::u64),
+        tag(":"),
+    )(input)?;
+    let (input, _) = multispace1(input)?;
+    let (input, _) = tag("Starting items: ")(input)?;
+    let (input, items) = separated_list1(tag(", "), nom::character::complete::u64)(input)?;
+    let (input, _) = multispace1(input)?;
+    let (input, _) = tag("Operation: new = old ")(input)?;
+    let (input, operation) = parse_operation(input)?;
+    let (input, _) = multispace1(input)?;
+    let (input, _) = tag("Test: divisible by ")(input)?;
+    let (input, divisible) = nom::character::complete::u64(input)?;
+    let (input, _) = multispace1(input)?;
+    let (input, _) = tag("If true: throw to monkey ")(input)?;
+    let (input, throw_val1) = nom::character::complete::u64(input)?;
+    let (input, _) = multispace1(input)?;
+    let (input, _) = tag("If false: throw to monkey ")(input)?;
+    let (input, throw_val2) = nom::character::complete::u64(input)?;
+    Ok((
+        input,
+        Monkey {
+            id,
+            items: VecDeque::from(items),
+            operation,
+            divisible,
+            throws: (Throw::True(throw_val1), Throw::False(throw_val2)),
+            thrown_count: 0,
+        },
+    ))
+}
+
+fn monkey_action(monkey: &mut Monkey, keep_calm: u64) -> Vec<(u64, u64)> {
+    let mut throws_items: Vec<(u64, u64)> = Vec::new();
+    for _ in 0..monkey.items.len() {
+        let item = monkey.items.pop_front().unwrap();
+        let worry_level = match monkey.operation {
+            Operation::Add(val) => match val {
+                (Value::Old, Value::Old) => (item + item) % keep_calm,
+                (Value::Old, Value::N(val)) => (item + val) % keep_calm,
+                (Value::N(_), _) => unreachable!(),
+            },
+            Operation::Mul(val) => match val {
+                (Value::Old, Value::Old) => (item * item) % keep_calm,
+                (Value::Old, Value::N(val)) => (item * val) % keep_calm,
+                (Value::N(_), _) => unreachable!(),
+            },
+        };
+
+        if worry_level % monkey.divisible == 0 {
+            match monkey.throws.0 {
+                Throw::True(val) => {
+                    throws_items.push((worry_level, val));
+                    monkey.thrown_count += 1;
+                }
+                _ => unreachable!(),
+            }
+        } else {
+            match monkey.throws.1 {
+                Throw::False(val) => {
+                    throws_items.push((worry_level, val));
+                    monkey.thrown_count += 1;
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+    throws_items
+}
+
+fn dispatch_throws_items(monkeys: &mut [Monkey], throws_items: Vec<(u64, u64)>) {
+    for (item, monkey_index) in throws_items {
+        monkeys[monkey_index as usize].items.push_back(item);
+    }
+}
+
+fn run(input: String) -> usize {
+    let (_, mut monkeys) = separated_list1(tag("\n\n"), parse_monkey)(&input).unwrap();
+    let keep_calm = monkeys
+        .iter()
+        .map(|monkey| monkey.divisible)
+        .product::<u64>();
+
+    for _ in 0..10000 {
+        for monkey_index in 0..monkeys.len() {
+            let throws_items = monkey_action(&mut monkeys[monkey_index], keep_calm);
+            dispatch_throws_items(&mut monkeys, throws_items);
+        }
+    }
+
+    monkeys.sort_by_key(|monkey| monkey.thrown_count);
+    dbg!(&monkeys);
+
+    monkeys
+        .iter()
+        .rev()
+        .take(2)
+        .map(|m| m.thrown_count)
+        .product()
+}
+
+fn main() {
+    let input = read_input(None);
+
+    let answer = run(input);
+
+    println!("Answer: {}", answer);
+}
+
+#[allow(unused_imports)]
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use indoc::indoc;
+    use pretty_assertions::{assert_eq, assert_ne};
+
+    #[test]
+    fn test_fake() {
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_parse_monkey() {
+        let input = indoc!(
+            "
+            Monkey 0:
+              Starting items: 79, 98
+              Operation: new = old * 19
+              Test: divisible by 23
+                If true: throw to monkey 2
+                If false: throw to monkey 3
+            "
+        );
+        let parsed = parse_monkey(input).unwrap();
+        assert_eq!(
+            parsed.1,
+            Monkey {
+                id: 0,
+                items: VecDeque::from(vec![79, 98]),
+                operation: Operation::Mul((Value::Old, Value::N(19))),
+                divisible: 23,
+                throws: (Throw::True(2), Throw::False(3)),
+                thrown_count: 0,
+            },
+        );
+    }
+
+    // #[test]
+    // fn test_monkey_action() {
+    //     let input = indoc!(
+    //         "
+    //         Monkey 0:
+    //           Starting items: 79, 98
+    //           Operation: new = old * 19
+    //           Test: divisible by 23
+    //             If true: throw to monkey 2
+    //             If false: throw to monkey 3
+    //         "
+    //     );
+    //     let mut parsed = parse_monkey(input).unwrap();
+    //     let throws_items = monkey_action(&mut parsed.1);
+    //     assert_eq!(parsed.1.items, []);
+    //     assert_eq!(throws_items, [(500, 3,), (620, 3,)]);
+    //     assert_eq!(parsed.1.thrown_count, 2);
+    // }
+
+    #[test]
+    fn test_run() {
+        let input = read_input(Some(indoc!(
+            "
+            Monkey 0:
+              Starting items: 79, 98
+              Operation: new = old * 19
+              Test: divisible by 23
+                If true: throw to monkey 2
+                If false: throw to monkey 3
+
+            Monkey 1:
+              Starting items: 54, 65, 75, 74
+              Operation: new = old + 6
+              Test: divisible by 19
+                If true: throw to monkey 2
+                If false: throw to monkey 0
+
+            Monkey 2:
+              Starting items: 79, 60, 97
+              Operation: new = old * old
+              Test: divisible by 13
+                If true: throw to monkey 1
+                If false: throw to monkey 3
+
+            Monkey 3:
+              Starting items: 74
+              Operation: new = old + 3
+              Test: divisible by 17
+                If true: throw to monkey 0
+                If false: throw to monkey 1
+                "
+        )));
+        dbg!(&input);
+        let answer = run(input);
+        //assert_eq!(answer, 10605);
+        assert_eq!(answer, 2713310158);
+    }
+}
